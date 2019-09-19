1eca5d2bac10f6f85f5c7cccac25ffca1a118af1
Gonchi <a20135298@pucp.pe>
2019-09-18 23:50:39 -0500

    menu reclamo return main menu

--- src/main/java/controllers/MenuController.java
+++ src/main/java/controllers/MenuController.java
@@ -9,6 +9,9 @@ import com.jfoenix.controls.JFXButton;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -20,6 +23,22 @@ import javafx.scene.control.Button;
  * @author flash
  */
 public class MenuController implements Initializable {   
+    
+    @FXML
+    private AnchorPane ap1;
+    
+    @FXML
+    void btnClickReclamos(ActionEvent event){
+        ap1.getChildren().clear();
+        AnchorPane nuevo = null;
+        try {
+            nuevo = FXMLLoader.load(getClass().getResource("/views/MenuReclamo.fxml"));
+        } catch (IOException ex) {
+            String error = ex.getMessage();
+            Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ap1.getChildren().add(nuevo); 
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
--- src/main/java/controllers/MenuReclamoController.java
+++ src/main/java/controllers/MenuReclamoController.java
@@ -5,9 +5,18 @@
  */
 package controllers;
 
+import java.awt.event.MouseEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -19,9 +28,23 @@ public class MenuReclamoController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+    @FXML
+    public AnchorPane apMenu;
+    
+    @FXML
+    public ImageView imgBack;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    @FXML
+    private void btnClickBack(ActionEvent event) throws IOException{  
+        apMenu.getChildren().clear();
+        AnchorPane nuevo = FXMLLoader.load(getClass().getResource("/views/Menu.fxml"));
+        apMenu.getChildren().add(nuevo); 
+    }
     
 }
