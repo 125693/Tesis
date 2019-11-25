b08c53485e63cf58daf41813dc1a5aaef902c6fb
Gonchi <a20135298@pucp.pe>
2019-11-25 11:34:04 -0500

    listado reclamos

--- src/main/java/Class/Reclamo.java
+++ src/main/java/Class/Reclamo.java
@@ -5,15 +5,121 @@
  */
 package Class;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author flash
  */
 public class Reclamo {
-    int clienteId;
-    int reclamoId;
-    int estadoId;
-    int fecha;
     
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+    private Connection con  = utils.ConnectionUtil.conDB();
+    
+    int id;
+    //int clienteId;
+    int reclamoId;
+    //int estadoId;
+    Date fecha;
+    Cliente cliente;
+    Estado estado;
+    List<ProductoReclamo> productos = new ArrayList<>();
+    
+    public Reclamo(){};
+    
+    public Reclamo(int id,int clienteId, int reclamoId, int estadoId, Date fecha) {
+        try {
+            this.id = id;
+            //this.clienteId = clienteId;
+            String sql = "";
+            sql = "SELECT * FROM cliente where id = ?";
+            preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setInt(1, clienteId);
+            resultSet = preparedStatement.executeQuery();
+            
+            while(resultSet.next()){
+                this.cliente = new Cliente(resultSet.getInt("id"), 
+                        resultSet.getString("Direccion"), 
+                        resultSet.getInt("Distrito_id"), 
+                        resultSet.getInt("TipoCliente_id"), 
+                        resultSet.getInt("Persona_id"));
+            }
+            this.reclamoId = reclamoId;
+            sql = "SELECT * FROM estado where id = ?";
+            preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setInt(1, estadoId);
+            resultSet = preparedStatement.executeQuery();
+            
+            while(resultSet.next()){
+                this.estado = new Estado(resultSet.getInt("id"), 
+                        resultSet.getString("Nombre"));
+            }
+            
+            sql = "SELECT * FROM reclamo_has_producto where reclamoId = ?";
+            preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+            
+            while(resultSet.next()){
+                this.estado = new Estado(resultSet.getInt("id"), 
+                        resultSet.getString("Nombre"));
+            }
+            
+            this.fecha = fecha;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Reclamo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getReclamoId() {
+        return reclamoId;
+    }
+
+    public void setReclamoId(int reclamoId) {
+        this.reclamoId = reclamoId;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
     
 }
--- src/main/java/controllers/ListaReclamosController.java
+++ src/main/java/controllers/ListaReclamosController.java
@@ -5,15 +5,33 @@
  */
 package controllers;
 
+import Class.Cliente;
+import Class.Reclamo;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -28,6 +46,21 @@ public class ListaReclamosController implements Initializable {
     
     @FXML
     public AnchorPane apWindow;
+    
+    @FXML private TreeTableView<Reclamo> tblReclamos;
+    @FXML private TreeTableColumn<Reclamo, Integer> colId;
+    @FXML private TreeTableColumn<Reclamo, Date> colFecha;
+    @FXML private TreeTableColumn<Reclamo, String> colDoc;
+    @FXML private TreeTableColumn<Reclamo, String> colEstado;
+    @FXML private TreeTableColumn<Reclamo, String> colNombre;
+    @FXML private TreeTableColumn<Reclamo, String> colFalla;
+    
+    //DB
+    Connection con = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+    
+    List<Reclamo> reclamos = new ArrayList<>();
     
     public void setapWindow(AnchorPane apWindow,AnchorPane apMenu) throws IOException
     {
@@ -50,6 +83,41 @@ public class ListaReclamosController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        LoadTableData();
     }    
+
+    private void LoadTableData() {
+        try {
+            // TODO
+            con  = utils.ConnectionUtil.conDB();
+            String sql = "SELECT * FROM reclamo";
+            preparedStatement = con.prepareStatement(sql); 
+            resultSet = preparedStatement.executeQuery();
+            
+            while(resultSet.next()){
+                Reclamo reclamo = new Reclamo(resultSet.getInt("id"), 
+                        resultSet.getInt("clienteId"), 
+                        resultSet.getInt("reclamoId"), 
+                        resultSet.getInt("estadoId"), 
+                        resultSet.getDate("fecha"));
+                reclamos.add(reclamo);
+            };
+            
+            TreeItem<Reclamo> rootNode = new TreeItem<>(reclamos.get(0));
+            for (Reclamo r: reclamos) {
+                TreeItem<Reclamo> node = new TreeItem<>(r);
+                rootNode.getChildren().add(node);
+            }
+            tblReclamos.setShowRoot(false);
+            tblReclamos.setRoot(rootNode);
+            colId.setCellValueFactory(new TreeItemPropertyValueFactory<>("id"));
+            colFecha.setCellValueFactory(new TreeItemPropertyValueFactory<>("fecha"));
+            colDoc.setCellValueFactory(cellData -> new SimpleStringProperty(String.valueOf(cellData.getValue().getValue().getCliente().getPersona().getId())));
+            colEstado.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getValue().getEstado().getNombre()));
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(ListaClientesController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
