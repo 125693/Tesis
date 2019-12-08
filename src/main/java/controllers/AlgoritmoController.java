93c2d2dfc7c7407401482ec36f802fb97b335b58
Gonchi <a20135298@pucp.pe>
2019-12-08 18:45:28 -0500

    algoritmo done

--- src/main/java/controllers/AlgoritmoController.java
+++ src/main/java/controllers/AlgoritmoController.java
@@ -15,10 +15,13 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 import static java.time.temporal.ChronoUnit.DAYS;
+import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -185,6 +188,9 @@ public class AlgoritmoController implements Initializable {
         //Algoritmo
         try
         {
+            List<Integer> ListaCandidatos;
+            long MaxDays = 3;
+            //Obtencion de planes
             long days = DAYS.between(dpFechaInicio.getValue(), dpFechaFin.getValue());
             days++;
             for (int i = 0; i < days; i++) {
@@ -198,25 +204,182 @@ public class AlgoritmoController implements Initializable {
                     int PlanId = -1;
                     while(resultSet.next()){
                         PlanId = resultSet.getInt("id");
-                    };
+                    }
                     PlanVisita plan = new PlanVisita(PlanId, dpFechaInicio.getValue().plusDays(i), tblTecnicos.getItems().get(j));
-                    if(plan.getHoras() < 8)
+                    if(plan.getHoras() < 9)
                         planesVisita.add(plan);
                 }
             }
-
+            //elegir tecino para cada reclamo
             for (int i = 0; i < rootNode.getChildren().size(); i++) {
-                System.out.println(rootNode.getChildren().get(i).getValue().getId());
+                Reclamo r = rootNode.getChildren().get(i).getValue();
+                Integer DistritoId = r.getCliente().getDistrito().getId();
+                Integer EspecialidadId = r.getProductos().get(0).getFalla().getEspecialidad_id();
+                Integer Tiempo = r.getProductos().get(0).getCantidad()*r.getProductos().get(0).getFalla().getTiempoResolucion();
+                if (r.getProductos().size() == 1)
+                {
+                    ListaCandidatos = GetListaCandidatos(DistritoId,EspecialidadId,Tiempo);
+                }
+                else
+                {
+                    boolean sameFalla = true;
+                    for (int j = 1; j < r.getProductos().size(); j++) {
+                        Tiempo += r.getProductos().get(j).getCantidad()*r.getProductos().get(j).getFalla().getTiempoResolucion();
+                        if(EspecialidadId != r.getProductos().get(j).getFalla().getEspecialidad_id())
+                            sameFalla = false;
+                    }
+                    if(sameFalla)
+                        ListaCandidatos = GetListaCandidatos(DistritoId,EspecialidadId,Tiempo);
+                    else
+                        ListaCandidatos = GetListaCandidatos(DistritoId,-1,Tiempo);
+                }
+                if(ListaCandidatos.size() == 0) break;
+                int index = SelectCandidato(ListaCandidatos);
+                planesVisita.get(index).getReclamos().add(r);
+                int horas = 0;
+                for (int j = 0; j < r.getProductos().size(); j++) {
+                   horas += r.getProductos().get(j).getCantidad()*r.getProductos().get(j).getFalla().getTiempoResolucion();
+                }
+                planesVisita.get(index).setHoras(planesVisita.get(index).getHoras()+horas);
             }
             
-//            for (int i = 0; i < tblTecnicos.getItems().size() ; i++) {
-//                System.out.println(tblTecnicos.getItems().get(i).getId());
-//            }
-            
+            //GUARDAR EN BD
+            for (int i = 0; i < planesVisita.size(); i++) {
+                if (planesVisita.get(i).getReclamos().size() != 0) {
+                    Integer PlanId = planesVisita.get(i).getId();
+                    String sql;
+                    if (planesVisita.get(i).getId() == -1) {
+                        sql = "INSERT INTO planvisita (fecha, tecnicoId) VALUES (?,?)";
+                        preparedStatement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                        preparedStatement.setDate(1,Date.valueOf(planesVisita.get(i).getFecha()));
+                        preparedStatement.setInt(2,planesVisita.get(i).getTecnico().getId());
+                        preparedStatement.executeUpdate();
 
+                        resultSet = preparedStatement.getGeneratedKeys();
+                        while(resultSet.next())
+                        {
+                           planesVisita.get(i).setId(resultSet.getInt(1));
+                        }
+                    }
+                    for (int j = 0; j < planesVisita.get(i).getReclamos().size(); j++) {
+                        sql = "SELECT * FROM planvisita_has_reclamo where reclamoId = ? and planVisitaId = ?";
+                        preparedStatement = con.prepareStatement(sql); 
+                        preparedStatement.setInt(1, planesVisita.get(i).getReclamos().get(j).getId());
+                        preparedStatement.setInt(2, planesVisita.get(i).getId());
+                        resultSet = preparedStatement.executeQuery();
+
+                        Boolean found = false;
+                        while(resultSet.next()){
+                            found = true;
+                        };
+                        if(!found)
+                        {
+                            sql = "INSERT INTO planvisita_has_reclamo (reclamoId, planVisitaId, estadoId) VALUES (?,?,1)";
+                            preparedStatement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                            preparedStatement.setInt(1,planesVisita.get(i).getReclamos().get(j).getId());
+                            preparedStatement.setInt(2,planesVisita.get(i).getId());
+                            preparedStatement.executeUpdate();
+
+                            sql = "UPDATE reclamo SET estadoId = 2 WHERE  (id = ?)";
+                            preparedStatement = con.prepareStatement(sql);
+                            preparedStatement.setInt(1,planesVisita.get(i).getReclamos().get(j).getId());
+                            preparedStatement.executeUpdate();
+                        }
+                    }
+                }
+            }
+            JOptionPane.showMessageDialog(null,"Reclamos asignados");
         } catch (SQLException ex) {
             Logger.getLogger(ListaClientesController.class.getName()).log(Level.SEVERE, null, ex);
         }
         
+    }
+
+    private List<Integer> GetListaCandidatos(Integer DistritoId, Integer EspecialidadId, Integer Tiempo) {
+        List<Integer> Candidatos = new ArrayList<Integer>();
+        for (int i = 0; i < planesVisita.size(); i++) {
+            //LocalDate PlanFecha = planesVisita.get(i).getFecha();
+            Integer Horas = planesVisita.get(i).getHoras();
+            if(planesVisita.get(i).getHoras() > 9) continue;
+            if (Horas + Tiempo > 9)
+                continue;
+            if(EspecialidadId != planesVisita.get(i).getTecnico().getEspecialidadId())
+                continue;
+            boolean match = false;
+            if(planesVisita.get(i).getReclamos().size() == 0) match = true;
+            for (Reclamo reclamo : planesVisita.get(i).getReclamos()) {
+                if(reclamo.getCliente().getDistrito().getId() == DistritoId)
+                    match = true;
+            }
+            if(!match) continue;
+            Candidatos.add(i);
+        }
+        if(Candidatos.size() != 0)
+            return Candidatos;
+        
+        for (int i = 0; i < planesVisita.size(); i++) {
+            Integer Horas = planesVisita.get(i).getHoras();
+            if(planesVisita.get(i).getHoras() > 9) continue;
+            if(EspecialidadId != planesVisita.get(i).getTecnico().getEspecialidadId())
+                continue;
+            boolean match = false;
+            if(planesVisita.get(i).getReclamos().size() == 0) match = true;
+            for (Reclamo reclamo : planesVisita.get(i).getReclamos()) {
+                if(reclamo.getCliente().getDistrito().getId() == DistritoId)
+                    match = true;
+            }
+            if(!match) continue;
+            Candidatos.add(i);
+        }
+        if(Candidatos.size() != 0)
+            return Candidatos;
+        
+        for (int i = 0; i < planesVisita.size(); i++) {
+            Integer Horas = planesVisita.get(i).getHoras();
+            if(planesVisita.get(i).getHoras() > 9) continue;
+            if(planesVisita.get(i).getTecnico().getEspecialidadId() != -1)
+                continue;
+            boolean match = false;
+            if(planesVisita.get(i).getReclamos().size() == 0) match = true;
+            for (Reclamo reclamo : planesVisita.get(i).getReclamos()) {
+                if(reclamo.getCliente().getDistrito().getId() == DistritoId)
+                    match = true;
+            }
+            if(!match) continue;
+            Candidatos.add(i);
+        }
+        if(Candidatos.size() != 0)
+            return Candidatos;
+        
+        for (int i = 0; i < planesVisita.size(); i++) {
+            Integer Horas = planesVisita.get(i).getHoras();
+            if(planesVisita.get(i).getHoras() > 9) continue;
+            boolean match = false;
+            if(planesVisita.get(i).getReclamos().size() == 0) match = true;
+            for (Reclamo reclamo : planesVisita.get(i).getReclamos()) {
+                if(reclamo.getCliente().getDistrito().getId() == DistritoId)
+                    match = true;
+            }
+            if(!match) continue;
+            Candidatos.add(i);
+        }
+        if(Candidatos.size() != 0)
+            return Candidatos;
+        
+        for (int i = 0; i < planesVisita.size(); i++) {
+            Integer Horas = planesVisita.get(i).getHoras();
+            if(planesVisita.get(i).getHoras() > 9) continue;
+            Candidatos.add(i);
+        }
+        
+        return Candidatos;
+    }
+
+    private int SelectCandidato(List<Integer> ListaCandidatos) {
+        Random rand = null;
+        int randomNum = (int) (Math.random()*ListaCandidatos.size()/5);
+        System.out.println(ListaCandidatos.size());
+        System.out.println(randomNum);
+        return ListaCandidatos.get(randomNum);
     }
 }
